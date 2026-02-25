280a73e0638292e3e09a921e064dcda3252d485b
armohovnikyan <armohovnikyan@gmail.com>
2026-02-25 22:12:06 +0400

    Initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Car : MonoBehaviour
+    {
+   
+    }
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] Car[] cars;
+    }   
+}
